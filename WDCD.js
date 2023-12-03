a589deee2d8020e5f2dd4ba8c9a5e1260c78ca14
NinhDuongCN <nghiaandeveloper@gmail.com>
2023-12-03 21:42:30 +0700

    2023.12.03. Add audio

--- WDCD.js
+++ WDCD.js
@@ -8,7 +8,7 @@ function PageLoading()
 
 //let WD = new Date(2026, 01, 22, 11, 11, 11, 111);
 //var WDsec = WD.getTime();
-var WDsec = (new Date(2024, 01, 20, 11, 11, 11, 111)).getTime();
+var WDsec = (new Date(2024, 1, 20, 11, 11, 11, 111)).getTime();
 
 function Countdown()
 {
@@ -27,14 +27,16 @@ function Countdown()
     document.getElementById("giay").innerText = secs < 10 ? ("0" + secs) : secs;
 }
 
-var backgroundCount = 35; //số lượng ảnh nền. NHỚ THÊM GIÁ TRỊ TRONG MẢNG tmplstAnh Ở HÀM CreateRandomLstAnh
+var backgroundCount = 63; //số lượng ảnh nền. NHỚ THÊM GIÁ TRỊ TRONG MẢNG tmplstAnh Ở HÀM CreateRandomLstAnh
 var id_changeBackground;
 var lstAnh = new Array();
 
 function CreateRandomLstAnh()
 {
     var tmplstAnh = [ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-                      20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34];
+                      20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+                      40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+                      60, 61, 62];
 
     for(var i = 0; i < backgroundCount; i++)
     {
